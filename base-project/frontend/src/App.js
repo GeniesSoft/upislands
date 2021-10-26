60088017b2631822d09a9727aa4dbbd2cae286b6
tt-p <tt-p@github.com>
2021-10-26 20:41:04 +0300

    Add template

--- base-project/frontend/src/App.js
+++ base-project/frontend/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import TeamContainer from "./containers/TeamContainer";
+import {ThemeProvider} from "styled-components";
+import theme from "./themes/default.theme";
+import GlobalStyles from "./themes/global.style";
+import {BrowserRouter} from "react-router-dom";
+import AuthProvider from "./context/AuthProvider";
+import Routes from "./router";
+import React from "react";
 
 function App() {
 
   return (
-    <div className={"temp"}>
-        <TeamContainer />
-    </div>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <AuthProvider>
+            <Routes />
+          </AuthProvider>
+        </BrowserRouter>
+      </ThemeProvider>
   );
 
 }
