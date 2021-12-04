5a8c127c49f22dc319de4d04d422eb1940b9aee5
MertYuksek <mert55yyyy@gmail.com>
2021-12-04 14:30:13 +0300

    Front end updated.

---
+++ base-project/frontend/src/service/auth/AuthApi.js
@@ -0,0 +1,44 @@
+import {BASE_URL} from "../../constants";
+import { ACCESS_TOKEN } from 'constants/index';
+
+export function signin(loginRequest) {
+    return request({
+        url: BASE_URL + "/auth/signin",
+        method: 'POST',
+        body: JSON.stringify(loginRequest)
+    });
+}
+
+export const getCurrentUser = () => {
+    if(!localStorage.getItem(ACCESS_TOKEN)) {
+        return Promise.reject("No access token set.");
+    }
+
+    return request({
+        url: BASE_URL + "/user/me",
+        method: 'GET'
+    });
+}
+
+const request = (options) => {
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+    })
+    
+    if(localStorage.getItem(ACCESS_TOKEN)) {
+        headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
+    }
+
+    const defaults = {headers: headers};
+    options = Object.assign({}, defaults, options);
+
+    return fetch(options.url, options)
+    .then(response => 
+        response.json().then(json => {
+            if(!response.ok) {
+                return Promise.reject(json);
+            }
+            return json;
+        })
+    );
+};
