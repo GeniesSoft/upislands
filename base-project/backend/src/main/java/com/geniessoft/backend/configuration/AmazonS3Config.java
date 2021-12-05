0572ca703b22e0f59be0e44f2ac42a904bf80381
ML-std <ML-std@github. com>
2021-12-05 13:24:56 +0300

    Problems solved

--- base-project/backend/src/main/java/com/geniessoft/backend/configuration/AmazonS3Config.java
+++ base-project/backend/src/main/java/com/geniessoft/backend/configuration/AmazonS3Config.java
@@ -14,8 +14,8 @@ public class AmazonS3Config {
     @Bean
     public AmazonS3 s3(){
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIASY5MARALSC6GRGUV",
-                "z1CnynSWldP50SVVHSvkOwOEcHHAYDtfqIopzYNu");
+                "",
+                "");
         return AmazonS3ClientBuilder
                 .standard()
                 .withRegion("eu-central-1")
